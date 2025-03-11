4e7c75095b8ee3224dc0aab736e1a3e4d9a1370a
Lee Millward <lee@m36.co.uk>
2025-03-11 15:54:52 +0000

    Update mail transport registration.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Mailer\Transport\Office365SmtpTransport;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Statamic\Statamic;
@@ -12,26 +13,23 @@ class AppServiceProvider extends ServiceProvider
   /**
    * Register any application services.
    */
-  public function register(): void
-  {
-    $this->app->extend('mail.manager', function ($manager) {
-      $manager->extend('office365', function ($config) {
-        return new Office365SmtpTransport(
-          $config['username'],
-          $config['client_id'],
-          $config['client_secret'],
-          $config['tenant_id'],
-          $config['refresh_token']
-        );
-      });
-    });
-  }
+  public function register(): void {}
 
   /**
    * Bootstrap any application services.
    */
   public function boot(): void
   {
+    Mail::extend('office365', function ($config) {
+      return new Office365SmtpTransport(
+        $config['username'],
+        $config['client_id'],
+        $config['client_secret'],
+        $config['tenant_id'],
+        $config['refresh_token']
+      );
+    });
+
     // Force HTTPS on production and staging
     if ($this->app->environment('production', 'preprod', 'staging')) {
       URL::forceScheme('https');
