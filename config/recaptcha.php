b1186f605343ad2af077c21ab240d1716be747fd
Lee Millward <lee@m36.co.uk>
2025-04-30 15:32:55 +0100

    Update reCAPTCHA to use X-Forwarded-For header for IP address if present.

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -21,7 +21,7 @@ return [
         'site_key' => env('RECAPTCHA_V3_SITE_KEY'),
         'secret_key' => env('RECAPTCHA_V3_SECRET_KEY'),
         'threshold' => env('RECAPTCHA_V3_THRESHOLD', .5),
-        'error_message' => 'Sorry, but reCAPTCHA has detected suspicious activity and identified you as a bot. If you believe this is an error, please contact us at: <a href="mailto:support@yourdomain.com" style="color: #007bff; text-decoration: underline;">support@yourdomain.com</a>',
+        'error_message' => 'Sorry, but reCAPTCHA has detected suspicious activity and identified you as a bot. If you believe this is an error, please contact us using the contact form available on this site.',
         'terms' => 'This website has implemented reCAPTCHA v3 and your use of reCAPTCHA v3 is subject to the <a href="https://www.google.com/policies/privacy/" target="_blank">Google Privacy Policy</a> and <a href="https://www.google.com/policies/terms/" target="_blank">Terms of Use</a>.',
 
         // In addition to performing the captcha verification when a form is submitted,
@@ -36,8 +36,8 @@ return [
     | v2 configuration
     |--------------------------------------------------------------------------
     |
-    | The default is the checkbox captcha, for which you can set the size to 
-    | either `normal` or `compact`. To enable the invisible reCAPTCHA, set the 
+    | The default is the checkbox captcha, for which you can set the size to
+    | either `normal` or `compact`. To enable the invisible reCAPTCHA, set the
     | size to `invisible`.
     |
     */
@@ -68,7 +68,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | You can exclude certain forms from reCAPTCHA validation by adding its
-    | handle below. For reCAPTCHA v3 you'll also need to add the CSS class 
+    | handle below. For reCAPTCHA v3 you'll also need to add the CSS class
     | "nocaptcha" to the <form> element.
     |
     */
